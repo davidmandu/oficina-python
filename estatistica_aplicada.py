4dd1a7251fe1a1b7b81d31f1414c9e68100abc2b
davidmandu <83472311+davidmandu@users.noreply.github.com>
2021-10-24 00:23:02 -0300

    Update estatistica_aplicada.py

--- estatistica_aplicada.py
+++ estatistica_aplicada.py
@@ -12,8 +12,10 @@
 
 # ##############PROCEDIMENTOS##############
 
-# ==============MÉDIA ARITMÉTICA SIMPLES==============
+#             1 - MÉDIA ARITMÉTICA SIMPLES
 def mediasimples(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
     media = 0
 
     for item in valores:
@@ -21,11 +23,13 @@ def mediasimples(valores=()):
 
     media = media / qtde_elementos
     print(f'Média Aritmética Simples = {media} ')
-######################################################
+#####################################################
 
 
-# ==============MÉDIA ARITMÉTICA PONDERADA==============
+#             2 - MÉDIA ARITMÉTICA PONDERADA
 def mediaponderada(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
     valorxpesos = 0
     soma_peso = 0
 
@@ -36,29 +40,101 @@ def mediaponderada(valores=()):
 
     media_ponderada = valorxpesos / soma_peso
     print(f'Média Aritmética Ponderada = {media_ponderada}')
-######################################################
+#####################################################
 
 
-# =##############INÍCIO DO PROGRAMA##############
+#             3 - MODA
+def moda(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
+    dic = {item: 0 for item in valores}
+    for item in valores:
+        if item in dic.keys():
+            dic[item] += 1
+    valor_moda = [item for item in dic.values()]
+    resultado_moda = [numero for numero in dic.keys() if dic[numero] == max(valor_moda) and max(valor_moda) > 1]
+
+    if len(resultado_moda) != 0:
+        print(f' Moda = {resultado_moda}')
+    else:
+        print('Amostra Amodal')
+#####################################################
+
+
+#             4 - MEDIANA
+def mediana(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
+    if qtde_elementos % 2 == 0:
+        med = (valores[int((qtde_elementos / 2) - 1)] + valores[int((qtde_elementos / 2))]) / 2
+        print(f'Mediana = {med}')
+    else:
+        med = valores[int((qtde_elementos / 2))]
+        print(f'Mediana = {med}')
+#####################################################
+
+
+#             5 - PRIMEIRO QUARTIL
+def primeiroquartil(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
+    if qtde_elementos % 4 == 0:
+        q1 = (valores[int((qtde_elementos / 4) - 1)] + valores[int((qtde_elementos / 4))]) / 2
+        print(f'Primeiro Quartil = {q1}')
+    else:
+        q1 = valores[int((qtde_elementos / 4))]
+        print(f'Primeiro Quartil = {q1}')
+#####################################################
+
+
+#             6 - TERCEIRO QUARTIL
+def terceiroquartil(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
+    if (3 * qtde_elementos) % 4 == 0:
+        q3 = (valores[int((3 * qtde_elementos / 4) - 1)] + valores[int((3 * qtde_elementos / 4))]) / 2
+        print(f'Terceiro Quartil = {q3}')
+    else:
+        q3 = valores[int(((3 * qtde_elementos) / 4))]
+        print(f'Terceiro Quartil = {q3}')
+#####################################################
+
+
+# #################INÍCIO DO PROGRAMA################
 qtde_elementos = int(input('Digite a quantidade de elementos da amostra: '))
 elementos = []
 for elemento in range(0, qtde_elementos):
     valor = int(input(f'Digite o {elemento+1}º valor: '))
     elementos.append(valor)
 
-# ==============MENU DE ESCOLHA==============
+# ==================MENU DE ESCOLHA==================
 print('''
                  ---MENU---
     | 1 - Média Aritmética Simples
     | 2 - Média Aritmética Ponderada
     | 3 - Moda
     | 4 - Mediana
+    | 5 - Primeiro Quartil
+    | 6 - Terceiro Quartil
+    | 7 - Amplitude
+    | 8 - Desvio-Padrão
+    | 9 - Calcular Tudo
+    | 0 - Encerrar
     
     ''')
 menu = int(input('Digite o número da Opção desejada: '))
 
-# ==============EXECUÇÃO DOS PROCEDIMENTOS==============
+# =============EXECUÇÃO DOS PROCEDIMENTOS============
 if menu == 1:
     mediasimples(elementos)
 elif menu == 2:
     mediaponderada(elementos)
+elif menu == 3:
+    moda(elementos)
+elif menu == 4:
+    mediana(elementos)
+elif menu == 5:
+    primeiroquartil(elementos)
+elif menu == 6:
+    terceiroquartil(elementos)
+
